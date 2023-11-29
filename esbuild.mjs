63407328796a9e64f9ae7f37e053933bc6496e28
André <39736248+andrefcdias@users.noreply.github.com>
2023-11-29 05:40:00 +0100

    chore: cleanup esbuild conf

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,15 +1,18 @@
 import * as esbuild from "esbuild";
 
+let ctx = await esbuild.context({
+  platform: "neutral",
+  entryPoints: ["src/*.ts"],
+  outdir: "dist/",
+  bundle: true,
+  allowOverwrite: true,
+  minify: false,
+});
+
 async function watch() {
-  let ctx = await esbuild.context({
-    platform: "neutral",
-    entryPoints: ["src/*.ts"],
-    outdir: "dist/",
-    bundle: true,
-    allowOverwrite: true,
-    minify: false,
-  });
   await ctx.watch();
-  console.log("Watching...");
+
+  console.log("Watching for changes...");
 }
+
 watch();
