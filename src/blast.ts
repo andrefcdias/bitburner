960a6b2530d61efccd0ab9914f5df0821113f936
André <39736248+andrefcdias@users.noreply.github.com>
2023-11-30 20:26:17 +0100

    feat(blast): add remote target support

--- src/blast.ts
+++ src/blast.ts
@@ -1,23 +1,29 @@
 import type { NS } from "@ns";
 
 export async function main(ns: NS): Promise<void> {
-  const [script, target] = ns.args;
+  const HOME_RAM_MARGIN = 5;
+  const [script] = ns.args;
+  const { target } = ns.flags([["target", "home"]]);
+  const parsedTarget = target as string;
 
   if (!script || typeof script !== "string") {
     ns.tprint("Please provide a script as first argument.");
     ns.exit();
   }
-  if (!target || typeof target !== "string") {
-    ns.tprint("Please provide a target host as second argument.");
-    ns.exit();
-  }
 
   const scriptCost = ns.getScriptRam(script);
-  const maxRam = ns.getServerMaxRam("home");
-  const usedRam = ns.getServerUsedRam("home");
-  const selfRam = ns.getScriptRam(ns.getScriptName());
+  const maxRam = ns.getServerMaxRam(parsedTarget);
+  const usedRam = ns.getServerUsedRam(parsedTarget);
+  const selfRam =
+    parsedTarget === "home" ? ns.getScriptRam(ns.getScriptName()) : 0;
   const availableRam = maxRam - usedRam + selfRam;
-  const threadsToUse = Math.floor(availableRam / scriptCost);
+  const threadsToUse =
+    Math.floor(availableRam / scriptCost) -
+    (parsedTarget === "home" ? HOME_RAM_MARGIN : 0);
 
-  ns.spawn(script, { spawnDelay: 100, threads: threadsToUse }, target);
+  if (parsedTarget === "home")
+    ns.spawn(script, { spawnDelay: 100, threads: threadsToUse });
+
+  ns.scp(script, parsedTarget);
+  ns.exec(script, parsedTarget, { threads: threadsToUse });
 }
