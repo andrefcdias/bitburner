100d6662ab0d7de7cc4b38f598e72401bcd07b24
André <39736248+andrefcdias@users.noreply.github.com>
2023-12-01 02:20:54 +0100

    feat(preparate): self-update after iterating all nodes

--- src/prepare.ts
+++ src/prepare.ts
@@ -2,41 +2,49 @@ import type { NS } from "@ns";
 import { getNodes } from "./lib/nodes";
 
 export async function main(ns: NS): Promise<void> {
-  const nodes = getNodes(ns);
   const { deep } = ns.flags([["deep", false]]);
 
-  for (const node of nodes) {
-    if (!node.hasAdminRights) continue;
+  let nodes = getNodes(ns);
+  for (let i = 0; i < nodes.length; i++) {
+    const node = nodes[i];
+    if (node.hasAdminRights) {
+      let estimatedSecDrop = 5;
+      let estimatedMoneyGrow = 0;
 
-    let estimatedSecDrop = 5;
-    let estimatedMoneyGrow = 0;
-    while (true) {
-      const currentMoneyLevel = ns.getServerMoneyAvailable(node.hostname);
-      if (!deep && currentMoneyLevel === 0) {
-        ns.print(`[ERROR] ${node.hostname} has no money`);
+      while (true) {
+        const currentMoneyLevel = ns.getServerMoneyAvailable(node.hostname);
+        if (!deep && currentMoneyLevel === 0) {
+          ns.print(`❌ ${node.hostname} has no money`);
+          break;
+        }
+
+        const minSecurityLevel = ns.getServerMinSecurityLevel(node.hostname);
+        const currentSecurityLevel = ns.getServerSecurityLevel(node.hostname);
+        const secDiff = currentSecurityLevel - minSecurityLevel;
+
+        if (secDiff - estimatedSecDrop > 0) {
+          estimatedSecDrop = await ns.weaken(node.hostname);
+          continue;
+        }
+
+        const maxMoneyLevel = ns.getServerMaxMoney(node.hostname);
+        const moneyDiff = maxMoneyLevel - currentMoneyLevel;
+
+        if (moneyDiff - estimatedMoneyGrow > 0) {
+          estimatedMoneyGrow = await ns.grow(node.hostname);
+          continue;
+        }
+
         break;
       }
 
-      const minSecurityLevel = ns.getServerMinSecurityLevel(node.hostname);
-      const currentSecurityLevel = ns.getServerSecurityLevel(node.hostname);
-      const secDiff = currentSecurityLevel - minSecurityLevel;
-
-      if (secDiff - estimatedSecDrop > 0) {
-        estimatedSecDrop = await ns.weaken(node.hostname);
-        continue;
-      }
-
-      const maxMoneyLevel = ns.getServerMaxMoney(node.hostname);
-      const moneyDiff = maxMoneyLevel - currentMoneyLevel;
-
-      if (moneyDiff - estimatedMoneyGrow > 0) {
-        estimatedMoneyGrow = await ns.grow(node.hostname);
-        continue;
-      }
-
-      break;
+      ns.print(`✅ ${node.hostname} is ready for hacking.`);
     }
 
-    ns.alert(`${node.hostname} is ready for hacking.`);
+    if (i === nodes.length - 1) {
+      ns.print(`🔄 Re-fetching node info...`);
+      nodes = getNodes(ns);
+      i = -1;
+    }
   }
 }
