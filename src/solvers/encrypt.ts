bc534de6a8e1477fa65c2b31d4df8e5aca1d92f5
André <39736248+andrefcdias@users.noreply.github.com>
2024-02-18 20:39:00 +0100

    feat: add solver + encrypt 3

---
+++ src/solvers/encrypt.ts
@@ -0,0 +1,73 @@
+const CHARS = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+type Map<T> = { [key: string]: T };
+
+const buildInnerCharMatrix = (offset: number) => {
+  const charMap: Map<string> = {};
+  const charStart = "A".charCodeAt(0);
+  const charEnd = "Z".charCodeAt(0);
+  for (let i = 0; i <= charEnd - charStart; i++) {
+    const offsetChars = CHARS.slice(offset).concat(CHARS);
+    charMap[String.fromCharCode(i + charStart)] = offsetChars[i];
+  }
+
+  return charMap;
+};
+
+const buildCharMatrix = () => {
+  const matrix: Map<Map<string>> = {};
+  const charStart = "A".charCodeAt(0);
+  const charEnd = "Z".charCodeAt(0);
+  for (let i = 0; i <= charEnd - charStart; i++) {
+    matrix[String.fromCharCode(i + charStart)] = buildInnerCharMatrix(i);
+  }
+
+  return matrix;
+};
+
+export const encrypt2 = (data: string) => {
+  const charMatrix = buildCharMatrix();
+  const [cypher, word] = data.split(" ");
+
+  const matchedCypher =
+    cypher.length >= word.length
+      ? cypher
+      : cypher.repeat(Math.ceil(word.length / cypher.length));
+
+  let result = "";
+  for (let i = 0; i < word.length; i++) {
+    const wordChar = word[i];
+    const cypherChar = matchedCypher[i];
+
+    result += charMatrix[wordChar][cypherChar];
+  }
+
+  return result;
+};
