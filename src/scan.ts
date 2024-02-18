753b0db851534cb5152d14099d6b48bd2e56afe8
André <39736248+andrefcdias@users.noreply.github.com>
2024-02-18 14:47:51 +0100

    feat: add detailed scan

--- src/scan.ts
+++ src/scan.ts
@@ -4,10 +4,43 @@ type ServerTree = {
   nodes: ServerTree[];
 } & Server;
 
-export async function main(ns: NS): Promise<void> {
-  const { target } = ns.flags([["target", ""]]);
-  const parsedTarget = target ? (target as string) : null;
+const colors = {
+  black: "\u001b[30m",
+  red: "\u001b[31m",
+  green: "\u001b[32m",
+  yellow: "\u001b[33m",
+  blue: "\u001b[34m",
+  magenta: "\u001b[35m",
+  cyan: "\u001b[36m",
+  white: "\u001b[37m",
+  brightBlack: "\u001b[30;1m",
+  brightRed: "\u001b[31;1m",
+  brightGreen: "\u001b[32;1m",
+  brightYellow: "\u001b[33;1m",
+  brightBlue: "\u001b[34;1m",
+  brightMagenta: "\u001b[35;1m",
+  brightCyan: "\u001b[36;1m",
+  brightWhite: "\u001b[37;1m",
+  reset: "\u001b[0m",
+};
 
+export async function main(ns: NS): Promise<void> {
+  const { target, analyze } = ns.flags([
+    ["target", ""],
+    ["analyze", false],
+  ]);
+  const parsedTarget = target ? (target as string) : null;
+  const parsedAnalyze = analyze ? (analyze as string) : null;
+
+  const humanizeMoney = (money: number) => {
+    const suffixes = ["", "K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc"];
+    let suffixIndex = 0;
+    while (money >= 1000) {
+      money /= 1000;
+      suffixIndex++;
+    }
+    return `${money.toFixed(2)}${suffixes[suffixIndex]}`;
+  };
   const scanRecursive = (target: string, parentNode?: string): ServerTree => ({
     ...ns.getServer(target),
     nodes: ns
@@ -18,11 +51,70 @@ export async function main(ns: NS): Promise<void> {
 
   const serverTree = scanRecursive("home");
 
-  const getPrintRecursive = (serverTree: ServerTree, depth = 0) => {
-    let printOutput = `${"  ".repeat(depth)}${serverTree.hostname}\n`;
+  const getPrintNode = (
+    content: string,
+    prefix?: string,
+    lastNode?: boolean,
+    header?: boolean
+  ) => {
+    let padder = "";
 
-    serverTree.nodes.forEach((server) => {
-      printOutput += getPrintRecursive(server, depth + 1);
+    if (header) {
+      if (lastNode) {
+        padder += `┗ `;
+      } else {
+        padder += `┣ `;
+      }
+      padder += parsedAnalyze ? colors.brightCyan : "";
+    } else {
+      if (lastNode) {
+        padder += `   `;
+      } else {
+        padder += `┃ `;
+      }
+    }
+
+    return `${prefix || ""}${padder} ${content}${colors.reset}\n`;
+  };
+
+  const getPrintRecursive = (
+    serverTree: ServerTree,
+    prefix?: string,
+    lastNode = false
+  ) => {
+    const lastOrHome = lastNode || serverTree.hostname === "home";
+    const padder = lastOrHome ? "   " : "┃ ";
+    let printOutput = getPrintNode(
+      serverTree.hostname,
+      prefix,
+      lastOrHome,
+      true
+    );
+
+    if (parsedAnalyze) {
+      let analyzeOutput = getPrintNode(
+        `Required hack: ${serverTree.requiredHackingSkill}`,
+        prefix,
+        lastOrHome
+      );
+      analyzeOutput += getPrintNode(
+        `Difficulty: ${Math.round(serverTree.hackDifficulty || 0)}`,
+        prefix,
+        lastOrHome
+      );
+      analyzeOutput += getPrintNode(
+        `Money: ${humanizeMoney(serverTree.hackDifficulty || 0)}`,
+        prefix,
+        lastOrHome
+      );
+      printOutput += analyzeOutput;
+    }
+
+    serverTree.nodes.forEach((server, i) => {
+      const isLastNode = i === serverTree.nodes.length - 1;
+
+      const newPrefix = `${prefix || ""}${padder}`;
+      printOutput += getPrintRecursive(server, newPrefix, isLastNode);
     });
 
     return printOutput;
