1f61a9e359b1d2546c86e936b36f5c568feee4d7
André <39736248+andrefcdias@users.noreply.github.com>
2024-02-28 17:59:40 +0100

    feat(assemble): solve contracts on rotation

--- src/assemble.ts
+++ src/assemble.ts
@@ -2,16 +2,23 @@ import type { NS } from "@ns";
 
 export async function main(ns: NS): Promise<void> {
   let timerDelay = 3 * 60 * 1000;
+  const scripts = ["solve.js", "crack.js", "hack-all.js"];
 
-  ns.exec("blast.js", "home", undefined, "prepare.js");
+  let maxCost = 0;
+  for (const script of scripts) {
+    maxCost = Math.max(ns.getScriptRam(script), maxCost);
+  }
+
+  ns.exec("blast.js", "home", undefined, "prepare.js", "--save", maxCost);
 
   while (true) {
     ns.exec("fetch-nodes.js", "home");
 
     await ns.sleep(3000);
 
-    ns.exec("crack.js", "home");
-    ns.exec("hack-all.js", "home");
+    for (const script of scripts) {
+      ns.exec(script, "home");
+    }
 
     await ns.sleep(timerDelay);
   }
